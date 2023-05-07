917de4a2b1baaf1d69cd420ddec712bec9305238
Desmond Nzubechukwu Abugu <nzubestdesmond@gmail.com>
2023-05-07 17:47:04 +0100

    new change

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { DesktopNav } from './components/Navlink/desktopviewNav';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { MobileNav } from './components/Navlink/mobileviewNav';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <BrowserRouter>
+      <div className="App">
+  <DesktopNav/>
+  <MobileNav/>
     </div>
+    </BrowserRouter>
+  
   );
 }
 
