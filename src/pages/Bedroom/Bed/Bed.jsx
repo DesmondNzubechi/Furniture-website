5aa6a010cfe99732e44d3e92ae037d55af0d2eeb
Desmond Nzubechukwu Abugu <nzubestdesmond@gmail.com>
2023-05-08 20:22:51 +0100

    new change

--- src/pages/Bedroom/Bed/Bed.jsx
+++ src/pages/Bedroom/Bed/Bed.jsx
@@ -73,11 +73,11 @@ Beds.map(items => (
 
 
     
-    <div className=" rounded relative flex  flex-col justify-center items-center    border my-[10px]  md:min-w-[150px] ">
-        <div className=" max-w-[300px]  h-full ">
-        <img src={items.Img} alt="" className="w-full h-full " />
+    <div className=" rounded relative flex  flex-col justify-center items-center w-full rounded   shadow-2xl my-[10px]   ">
+        <div className=" max-w-[300px] m-0  h-full ">
+        <img src={items.Img} alt="" className="w-full p-0 h-full " />
         </div>
-       <div className="flex w-full  bottom-0 left-0 right-0  bg-black p-2 rounded flex-row justify-between ">
+       <div className="flex w-full  bottom-0 left-0 right-0  bg-black p-2  flex-row justify-between ">
             <div>
             <h1 className="md:text-[17px] text-[12px] text-white font-fonty uppercase ">{items.name}</h1>
             <p className=" top-2 left-2 text-[12px] self-start font-bold  text-slate-50 flex flex-row gap-2 "> <span className=" text-slate-500 line-through ">${items.discount}</span>${items.Price} <span></span></p>
@@ -85,7 +85,7 @@ Beds.map(items => (
             </div>
           
             <div className="flex flex-row gap-2 items-center">
-            <AiFillHeart className="text-[20px] p-1  text-red-500 shadow rounded-full md:text-[30px]  "/>
+            <AiFillHeart className="text-[20px]   text-red-500 shadow rounded-full md:text-[30px]  "/>
     <AiFillShopping className="text-[20px] p-1 text-white bg-slate-500 rounded-full  md:text-[35px] "/>
             </div>
         </div>
